5496af44d5a6fd9cd1acabe4ce9cf2a3ffabadc4
Hummingbird1986.local <danieldeng@me.com>
2020-10-29 12:10:19 +0800

    add termios configuration

--- synctime.c
+++ synctime.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 #include <time.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <termios.h>
+#include <unistd.h>
+
 #define CMD_START 0xFE
 #define CMD_END 0xFD
 #define RIG_ADDR 0x94
@@ -9,9 +15,49 @@
 #define TIME_RW1 0x00
 #define TIME_RW2 0x95
 
-int main(void)
-  
- { struct tm *ptr;
+
+
+
+int main(void){
+
+int serial_port = open("/dev/cu.xxxx", O_RDWR);
+struct termios tty;
+
+if(tcgetattr(serial_port, &tty) != 0) {
+
+  printf("Error %i from tcgetattr: %s\n", errno, strerror(errno));
+  return 1;
+}
+
+  tty.c_cflag &= ~PARENB; // Clear parity bit, disabling parity (most common)
+  tty.c_cflag &= ~CSTOPB; // Clear stop field, only one stop bit used in communication (most common)
+  tty.c_cflag &= ~CSIZE; // Clear all bits that set the data size 
+  tty.c_cflag |= CS8; // 8 bits per byte (most common)
+  tty.c_cflag &= ~CRTSCTS; // Disable RTS/CTS hardware flow control (most common)
+  tty.c_cflag |= CREAD | CLOCAL; // Turn on READ & ignore ctrl lines (CLOCAL = 1) 
+  tty.c_lflag &= ~ICANON;
+  tty.c_lflag &= ~ECHO; // Disable echo
+  tty.c_lflag &= ~ECHOE; // Disable erasure
+  tty.c_lflag &= ~ECHONL; // Disable new-line echo
+  tty.c_lflag &= ~ISIG; // Disable interpretation of INTR, QUIT and SUSP
+  tty.c_iflag &= ~(IXON | IXOFF | IXANY); // Turn off s/w flow ctrl
+  tty.c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL); // Disable any special handling of received bytes
+  tty.c_oflag &= ~OPOST; // Prevent special interpretation of output bytes (e.g. newline chars)
+  tty.c_oflag &= ~ONLCR; // Prevent conversion of newline to carriage return/line feed
+   tty.c_cc[VTIME] = 10;    // Wait for up to 1s (10 deciseconds), returning as soon as any data is received.
+  tty.c_cc[VMIN] = 0;
+
+  // Set in/out baud rate to be 9600
+  cfsetispeed(&tty, B9600);
+  cfsetospeed(&tty, B9600);
+
+  // Save tty settings, also checking for error
+  if (tcsetattr(serial_port, TCSANOW, &tty) != 0) {
+      printf("Error %i from tcsetattr: %s\n", errno, strerror(errno));
+      return 1;
+  }
+
+  struct tm *ptr;
   time_t epochTime;
   epochTime=time(NULL);
   ptr=localtime(&epochTime);
